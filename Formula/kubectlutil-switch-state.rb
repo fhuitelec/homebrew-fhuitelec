1cd56c92e1ff53892736b34187d057c7f95cc346
Fabien Huitelec <fabien@huitelec.fr>
2019-03-17 17:26:31 +0100

    chores: rename kubectl-switch-state to kubectlutil-switch-state

--- Formula/kubectlutil-switch-state.rb
+++ Formula/kubectlutil-switch-state.rb
@@ -1,18 +1,18 @@
-class KubectlSwitchState < Formula
+class KubectlutilSwitchState < Formula
     desc "Kubernetes state control that segregates contexts to enable or disable kubectl usage"
     homepage "https://github.com/fhuitelec/kubectl-tools"
-    url "https://github.com/fhuitelec/kubectl-tools/archive/${FORMULA_VERSION}.tar.gz"
-    sha256 "${SHA_SUM}"
-    version "${FORMULA_VERSION}"
+    url "https://github.com/fhuitelec/kubectl-tools/archive/0.1.0.tar.gz"
+    sha256 "f3430f175ec78d0bd0fe97c934e6f3b129def2313acb493ee6722fe89706409d"
+    version "0.1.0"
     head "https://github.com/fhuitelec/kubectl-tools.git"
 
     depends_on "kubernetes-cli" => :optional
 
     def install
-      bin.install "bin/kubectl-switch-state"
+      bin.install "bin/kubectlutil-switch-state"
     end
 
     test do
-      system "which", "kubectl-switch-state"
+      system "which", "kubectlutil-switch-state"
     end
   end
--- formula-templates/kubectlutil-switch-state.rb
+++ formula-templates/kubectlutil-switch-state.rb
@@ -1,18 +1,18 @@
-class KubectlSwitchState < Formula
+class KubectlutilSwitchState < Formula
     desc "Kubernetes state control that segregates contexts to enable or disable kubectl usage"
     homepage "https://github.com/fhuitelec/kubectl-tools"
-    url "https://github.com/fhuitelec/kubectl-tools/archive/0.1.0.tar.gz"
-    sha256 "183e745503a01ab59f23f8426093d316744d6a1daf0f2b220bea350ee87599b6"
-    version "0.1.0"
+    url "https://github.com/fhuitelec/kubectl-tools/archive/${FORMULA_VERSION}.tar.gz"
+    sha256 "${SHA_SUM}"
+    version "${FORMULA_VERSION}"
     head "https://github.com/fhuitelec/kubectl-tools.git"
 
     depends_on "kubernetes-cli" => :optional
 
     def install
-      bin.install "bin/kubectl-switch-state"
+      bin.install "bin/kubectlutil-switch-state"
     end
 
     test do
-      system "which", "kubectl-switch-state"
+      system "which", "kubectlutil-switch-state"
     end
   end
